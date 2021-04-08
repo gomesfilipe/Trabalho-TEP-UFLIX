fa2702ed65cc4a887a9c6a0f86dce8700f41bf45
gomesfilipe <filipejvmf@gmail.com>
2021-04-08 12:11:57 -0300

    resolver problema da escirta no csv urgente

--- include/usuario.h
+++ include/usuario.h
@@ -5,6 +5,47 @@
 
 typedef struct usuario tUsuario;
 
+/**
+ * @brief Cadastra um novo usuário na UFLIX. 
+ * Esta função apenas escreve no arquivo csv de usuários os dados referentes ao cadastro, caso seja válido.
+ * @param login Login do usuário.
+ * @param senha Senha do usuário.
+ * @param confirmaSenha String para verificação se esta e o parâmetro anterior são iguais.
+ * @param fileName Caminho para o arquivo csv de usuários.
+ * @return Função sem retorno.
+ **/
 void cadastraUsuario(char *login, char *senha, char *confirmaSenha, char *fileName);
+
+/**
+ * @brief Efetua o login de um usuário para acesso ao UFLIX.
+ * Esta função faz a verificação se existe no arquivo CSV um par de login e senha iguais aos parâmetros.
+ * @param login Login inserido pelo usuário.
+ * @param senha senha inserida pelo usuário.
+ * @param fileName Caminho para o arquivo CSV de usuários.
+ * @return 1, caso o login seja válido e 0 caso o login seja inválido, seja devido ao login não estar cadastrado ou devido a senha estar incorreta.
+ **/
+int efetuaLogin(char *login, char *senha, char *fileName);
+
+/**
+ * @brief Inativa a conta de um usuário (como se fosse excluir a conta).
+ * @param usuario Ponteiro para um usuário.
+ * @return Ponteiro de usuário com o campo referente a conta estar ativa ou não atualizado.
+ **/ 
+tUsuario* inativarConta(tUsuario *usuario);
+
+/**
+ * @brief Aloca memória para um ponteiro de usuário e seus campos, e os inicializa.
+ * @param login Login do usuário.
+ * @param senha Senha do usuário.
+ * @param hist Histórico do usuário.
+ * @return Ponteiro de usuário com memória alocada e inicializado.
+ **/
+tUsuario* criaUsuario(char *login, char *senha);
+
+/**
+ * @brief Libera memória de um ponteiro de usuário.
+ * @param usuario Usuario que será liberado da memória.
+ **/
+tUsuario* destroiUsuario(tUsuario *usuario);
 
 #endif
--- src/usuario.c
+++ src/usuario.c
@@ -8,34 +8,79 @@ struct usuario{
     int ativo; // 1 ta ativo e 0 inativo
 };
 
-int efetuaLogin(){
+int efetuaLogin(char* login, char* senha, char* fileName){
+    char *loginAux = (char*) malloc(sizeof(char) * 100);
+    char *senhaAux = (char*) malloc(sizeof(char) * 100);
+
+   FILE *f = fopen(fileName, "r");
+    if(f== NULL){
+    printf("Erro na abertura do arquivo!\n");
+    exit(1);
+    }
     
+    for(int i=0; i < contaLinhasCSV(fileName); i++){
+        fscanf(f, "%[^,],%[^\n]\n" , loginAux, senhaAux); 
+        fscanf(f, "%*[^,]" );
+        if(strcmp(login, loginAux)== 0){ //login existe
+            if(strcmp(senha, senhaAux )== 0){
+               return 1; //vai para o menu pricipal, fazemos isso na main
+            }
+            else{
+                printf("Senha incorreta!\n");
+                return 0;
+            }
+        }  
+    } 
+    printf("Usuário não cadastrado!\n");
+
+    free(loginAux);
+    free(senhaAux);
 }
 
+//se tem uma conta inativa, o cadastro pode ter o mesmo uusario essa conta, acrescentar isso NAO ESQUECER
 //cadastra um usuario por vez
 void cadastraUsuario(char *login, char *senha, char *confirmaSenha, char *fileName){
-    int aux = 0;
+    int encerra = 0; //Essa variavel é setada quando encontramos alguma condição que impede a continuação do cadastro. 
     char *loginAux = (char*) malloc(sizeof(char) * 100);
+    
+    for(int i = 0; i < strlen(login); i++){ 
+        if(!isalnum(login[i])){ // Verificando se login é alfanumérico
+            printf("Login deve ser alfanumerico!\n");  
+            encerra = 1;
+            break;
+        }
+    }
+    if(encerra == 0){ 
+        for(int i = 0; i < strlen(senha); i++){ 
+            if(!isalnum(senha[i])){ // Verificando se senha é alfanumérica
+                printf("Senha deve ser alfanumerica!\n");  
+                encerra = 1;
+                break;
+            }
+        } 
+    }
     FILE *f = fopen(fileName, "r+");
     if(f == NULL){
         printf("Erro na abertura do aruqivo!\n");
         exit(1);
     }
     
-    for(int i = 0; i < contaLinhasCSV(fileName); i++){
-        fscanf(f, "%[^,],", loginAux);
-        if(strcmp(login, loginAux) == 0){ //se forem iguais
-            printf("Login ja existente!\n");
-            aux = 1;
-            break;
-            
-        } else{
-            fscanf(f, "%*[^\n]\n");
-            
-        }
-    } //se passou do for é porque é um login que ainda não tem, ou seja, um novo usuário
+    if(encerra == 0){   
+        for(int i = 0; i < contaLinhasCSV(fileName); i++){
+            fscanf(f, "%[^,],", loginAux);
+            if(strcmp(login, loginAux) == 0){ //se forem iguais
+                printf("Login ja existente!\n");
+                encerra = 1;
+                break;
+                
+            } else{
+                fscanf(f, "%*[^\n]\n");
+                
+            }
+        } //se passou do for é porque é um login que ainda não tem, ou seja, um novo usuário
+    }
 
-    if(aux == 0){
+    if(encerra == 0){
         if(strcmp(senha, confirmaSenha) == 0){ //se senha e confirma senha forem iguais
             fprintf(f, "%s,%s\n", login, senha);
             printf("Cadastro feito com sucesso!\n");
@@ -44,18 +89,57 @@ void cadastraUsuario(char *login, char *senha, char *confirmaSenha, char *fileNa
         }
     }
 
+    free(loginAux);
     fclose(f);
 }
 
-
-
-tUsuario inativarConta(){
-
+tUsuario* inativarConta(tUsuario *usuario){
+    usuario->ativo = 0; //inativando usuario
+    return usuario;
 }
 
+tUsuario* criaUsuario(char *login, char *senha){
+    tUsuario *usuario = (tUsuario*) malloc(sizeof(tUsuario));
+    usuario->login = login;
+    usuario->senha = senha;
+    int qtdFilmesAtual = contaFilmesNoHistoricoCSV();
+    if(qtdFilmesAtual == 0){ //consideramos aqui que conseguimos escrever o filme na frente do login e da sala
+        usuario->hist = criaPrimeiroHistorico();
+    } else{
+        // fazer alguma coisa que leia o csv de historico e capture os dados que sao os parametros da funcao abaixo 
+        usuario->hist = resgataHistorico(qtdFilmesAtual, data, nota);
+    }
+    // chamar função de criar historico ou resgatahistorico
+    usuario->ativo = 1;
+    return usuario;
+}
+char *login; // vetor de caracteres
+    char *senha; // vetor de caracteres
+    tHistorico *hist; // único elemento
+    int ativo; // 1 ta ativo e 0 inativo
+tUsuario* destroiUsuario(tUsuario usuario){
+    
+}
 
-//TODO cadastrar usuario
-//TODO ativar e inativar conta
+// char *login; // vetor de caracteres
+// char *senha; // vetor de caracteres
+// tHistorico *hist; // único elemento
+// int ativo; // 1 ta ativo e 0 inativo
+
+
+//TODO cadastrar usuario 
+//TODO ativar e inativar conta ok
 //TODO sair da conta
+//TODO criar usuario ok
+//TODO criar funções para desalocar filme, historico, data e usuario
 
 
+
+//roteiro
+//csv
+//testar funcao resgata no filme ou no historico
+//usuario
+//makefile
+//main 
+//funcao em cada TAD para destruir a TAD
+//verbosidade
